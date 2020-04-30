912a2c9924bdd7593539eac721577542ac9c3766
hirofumitorato <hirofumitorato@gmail.com>
2020-04-30 10:17:58 +0900

    iii

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -82,7 +82,7 @@ $(function(){
         var insertHTML = '';
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
+          insertHTML += buildHT ML(message)
         });
         //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,4 @@
 class Api::MessagesController < ApplicationController
-
   def index
     # ルーティングでの設定によりparamsの中にgroup_idというキーでグループのidが入るので、これを元にDBからグループを取得する
     group = Group.find(params[:group_id])
